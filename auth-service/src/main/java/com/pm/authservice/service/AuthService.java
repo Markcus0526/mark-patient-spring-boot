20aef988f3e47a074736781c0dccf40fd02107bc
markcus <markcus0526@proton.me>
2026-02-26 02:16:39 +0100

    Update api-gateway

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -1,11 +1,13 @@
 package com.pm.authservice.service;
 
+
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
+import com.pm.authservice.repository.UserRepository;
 import com.pm.authservice.util.JwtUtil;
 import io.jsonwebtoken.JwtException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,29 +20,28 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
-    private static final Logger log = LoggerFactory.getLogger(
-            AuthService.class);
-
-    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+    public AuthService(UserService userService,  PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
-        this.jwtUtil = jwtUtil;
+        this.jwtUtil= jwtUtil;
     }
 
-    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
-        Optional<String> token = userService
-                .findByEmail(loginRequestDTO.getEmail())
-                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
-                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO){
+
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(u->passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u->jwtUtil.generateToken(u.getEmail(),u.getRole()));
 
         return token;
+
     }
 
-    public boolean validateToken(String token) {
-        try {
+    public boolean validateToken(String token){
+        try{
             jwtUtil.validateToken(token);
             return true;
-        } catch (JwtException e) {
+        }catch (JwtException e){
             return false;
         }
     }
